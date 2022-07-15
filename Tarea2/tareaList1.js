927e114cf8a6708299c15d723a6f19cbdb6ac731
FranciscoSith1 <f.cidsegovia@gmail.com>
2022-07-15 08:57:36 -0400

    Tarea de lista

---
+++ Tarea2/tareaList1.js
@@ -0,0 +1,18 @@
+const nombre = "Francisco";
+
+const edad = 25;
+
+const desarrollador = true;
+
+const fNacimiento = new Date(1996,09,01);
+
+const libro = {
+    titulo: "Quique Hache Detective",
+    autor: "Sergio Gómez",
+    fecha: new Date (1999),
+    url: "https://www.curriculumnacional.cl/estudiantes/Educacion-General/Lenguaje-y-comunicacion-Lengua-y-literatura/Lengua-y-literatura-7-basico/224070:Libro-Quique-Hache-detective"
+}
+
+const tareaList = [nombre, edad, desarrollador,fNacimiento,libro];
+
+console.log(libro.tareaList);
